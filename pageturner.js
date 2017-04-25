e7e1239c5a6a1ae938ce9acb54ac166530c64daf
Ole Martin <ole.martin@geta.no>
2017-04-25 17:14:24 +0200

    Pretty much complete and ready for use.

--- pageturner.js
+++ pageturner.js
@@ -2,25 +2,41 @@ function sleep (ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-async function openThese(urls){
-	open(urls.shift(), "PageTurner")
-//	await sleep(5000)
-	await sleep(5000)
-	if (urls.length > 0) openThese(urls)
+async function openThese(urls, delay){
+	console.log()
+	open(urls.shift(), "PageTurner")	
+	await sleep(delay)
+	if (urls.length > 0) openThese(urls, delay)
 }
 
 let pageturner = {
-	init: function(input, output) {
+	init: function(input, separator, delay) {
 		pageturner.input = input;
+		pageturner.output = output || window.getElementById('output')
+		pageturner.delay = delay || 5000;
+
 		input.addEventListener('change', this.openFile)
 	},
 
-	openFile: async function (event) {
+	openFile: async function (event, delay = 5000) {
 	    let reader = new FileReader();
 	    reader.onload = file => {
-    		pageturner.urls = file.target.result.split(";")
-    		openThese(pageturner.urls);
+    		urls = file.target.result.split(';')
+    		pageturner.makeListOf(urls)
+    		openThese(urls, pageturner.delay)
     	}
-    	reader.readAsText(pageturner.input.files[0], "UTF-8")
+    	reader.readAsText(pageturner.input.files[0], "UTF-8")    	
+	},
+
+	makeListOf(urls){
+		let list = document.createElement('UL')
+		urls.map(url => {
+			console.log(url)
+			let item = document.createElement('LI');
+			let text = document.createTextNode(url);
+			item.appendChild(text)
+			list.appendChild(item)
+		})
+		window.output.appendChild(list)
 	}
 }
